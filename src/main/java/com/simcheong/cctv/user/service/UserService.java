e88e9d747d234b3a475d387d8b9c426f1a9b429c
CheongEun <ce090404@naver.com>
2024-05-15 22:27:13 +0900

    :pencil2: fix typo

--- src/main/java/com/simcheong/cctv/user/service/UserService.java
+++ src/main/java/com/simcheong/cctv/user/service/UserService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-    private  PasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
     private UserRepository userRepository;
 
     @Transactional
