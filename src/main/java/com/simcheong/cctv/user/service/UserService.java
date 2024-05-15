4fe70a0695a0fe7ab28bf897cc333e7ad12984a3
CheongEun <ce090404@naver.com>
2024-05-15 17:41:07 +0900

    :sparkles: UserService file 추가

---
+++ src/main/java/com/simcheong/cctv/user/service/UserService.java
@@ -0,0 +1,20 @@
+package com.simcheong.cctv.user.service;
+
+import com.simcheong.cctv.user.dto.UserRequest;
+import com.simcheong.cctv.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private  PasswordEncoder passwordEncoder;
+    private UserRepository userRepository;
+
+    @Transactional
+    public void signUp(final UserRequest.SignUpDTO requestDTO) {
+        requestDTO.encodePassword(passwordEncoder.encode(requestDTO.getPassword()));
+        userRepository.save(requestDTO.mapToEntity());
+    }
+}
