4d3e514fb1c0c014f8440d4041e9bb3fbd212046
CheongEun <ce090404@naver.com>
2024-06-08 23:36:37 +0900

    :lock: Update SecurityConfig to include JWT configuration

--- src/main/java/com/simcheong/cctv/_core/config/SecurityConfig.java
+++ src/main/java/com/simcheong/cctv/_core/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.simcheong.cctv._core.config;
 
+import com.simcheong.cctv._core.security.JwtAuthenticationFilter;
+import com.simcheong.cctv._core.security.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,12 +9,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.Customizer;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -21,14 +29,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtTokenProvider);
+
         http
-                .cors(withDefaults()) // CORS 설정
+                .cors(Customizer.withDefaults()) // CORS 설정
+                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/api/signup", "/api/login").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .csrf(csrf -> csrf.disable()); // CSRF 보호 비활성화
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // JWT 인증 필터 추가
 
         return http.build();
     }
