d880ebf1080fd0256c338aba539381c14a204b05
CheongEun <ce090404@naver.com>
2024-06-08 23:36:47 +0900

    :card_file_box: Update Shop domain with additional fields

--- src/main/java/com/simcheong/cctv/shop/domain/Shop.java
+++ src/main/java/com/simcheong/cctv/shop/domain/Shop.java
@@ -1,23 +1,48 @@
 package com.simcheong.cctv.shop.domain;
 
+import com.simcheong.cctv._core.utils.AuditingFields;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
-@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
-
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Shop {
+@Table(name = "\"shop\"") // 테이블 이름을 역따옴표로 감쌈
+public class Shop extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long shopId;
 
+    @Setter
+    @Column(nullable = false, length = 100, name = "name")
+    private String name;
 
+    @Setter
+    @Column(nullable = false, length = 255, name = "address")
+    private String address;
+
+    @Setter
+    @Column(name = "rtsp_url")
+    private String rtspUrl;
+
+    @Builder
+    public Shop(Long shopId, String name, String address, String rtspUrl) {
+        this.shopId = shopId;
+        this.name = name;
+        this.address = address;
+        this.rtspUrl = rtspUrl;
+    }
 }
