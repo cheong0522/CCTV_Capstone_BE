51a5451e22f83a7bd310b22b82ca28d56daa5210
CheongEun <ce090404@naver.com>
2024-06-08 23:36:23 +0900

    :heavy_plus_sign: Add dependencies for JWT support in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,10 @@ dependencies {
 	testImplementation("org.springframework.security:spring-security-test:4.1.4.RELEASE")
 
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
+	implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 }
 
 tasks.withType<Test> {
