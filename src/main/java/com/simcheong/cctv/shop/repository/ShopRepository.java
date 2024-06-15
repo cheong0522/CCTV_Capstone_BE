bb3e6f646c2ce41c98b237a8042aba7c2adf5de5
CheongEun <ce090404@naver.com>
2024-06-15 21:08:21 +0900

    :zap: Optimize ShopRepository queries

--- src/main/java/com/simcheong/cctv/shop/repository/ShopRepository.java
+++ src/main/java/com/simcheong/cctv/shop/repository/ShopRepository.java
@@ -3,5 +3,11 @@ package com.simcheong.cctv.shop.repository;
 import com.simcheong.cctv.shop.domain.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ShopRepository extends JpaRepository<Shop, Long> {
+    List<Shop> findAll();
+
+    Optional<Shop> findById(Long shopId);
 }
