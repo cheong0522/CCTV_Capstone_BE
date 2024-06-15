3b88fb79e36bf65c920ca7c2607f0f91f4f21101
CheongEun <ce090404@naver.com>
2024-06-15 21:09:10 +0900

    :card_index: Update User domain model

--- src/main/java/com/simcheong/cctv/user/domain/User.java
+++ src/main/java/com/simcheong/cctv/user/domain/User.java
@@ -1,24 +1,14 @@
 package com.simcheong.cctv.user.domain;
 
 import com.simcheong.cctv._core.utils.AuditingFields;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Getter
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "\"user\"") // 테이블 이름을 큰따옴표로 감쌈
+@Table(name = "\"user\"")
 public class User extends AuditingFields {
 
     @Id
@@ -27,15 +17,16 @@ public class User extends AuditingFields {
     private Long userId;
 
     @Setter
-    @Column(nullable = false, length = 15, name = "nickname")
+    @Column(nullable = false, length = 45, name = "nickname")
     private String nickname;
 
     @Column(nullable = false, name = "password")
     private String password;
 
-    @Column(length = 11, name = "phone_number")
+    @Column(length = 15, name = "phone_number")
     private String phoneNumber;
 
+    @Setter
     @Column(name = "refresh_token")
     private String refreshToken;
 
